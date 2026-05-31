ee9c9211cb930efb0d6d65ec9d5b2c5f01a5787b
SergBul85 <donetsk55555@gmail.com>
2026-05-31 06:56:15 +0300

    Init commit

---
+++ src/main/java/com/hstn/validdemo/ValiddemoApplication.java
@@ -0,0 +1,13 @@
+package com.hstn.validdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hstn/validdemo/ValiddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hstn.validdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValiddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
